a5e2146229f3664e8996507a9d2c6e1b84f95572
florian.rifflart <florian.rifflart@gmail.com>
2017-10-12 09:37:05 -0400

    correction erreur "rake aborted"#

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171012122511) do
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.date     "birthday"
+    t.string   "surname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
